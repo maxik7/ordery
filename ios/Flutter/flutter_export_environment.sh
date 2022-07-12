76b9dfa4a43bd18ed4fac656a94f17c333f631db
maxik7 <kammerer.maxi@gmail.com>
2022-07-12 11:34:25 +0200

    Init

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/maxi/Desktop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/maxi/Desktop/DiMa/RestaurantAppUIKit-master"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/maxi/Desktop/DiMa/RestaurantAppUIKit-master/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=5.0.0"
+export "FLUTTER_BUILD_NUMBER=50"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/maxi/Desktop/DiMa/RestaurantAppUIKit-master/.dart_tool/package_config.json"
